f446ec41c814da921b8e0045454b2e01c6be1b65
Yaoxing-chen <thewindofstar@gmail.com>
2023-11-07 08:19:02 +1100

    Update: Update the models.py and database for new table
    Merged two tables challenge and status, updated related FK.

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ class ToDo(Base):
     completed = Column(Boolean, default=False)
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "User"
 
     id = Column(Integer, primary_key=True, index=True)
     firebase_uid = Column(String, nullable=False)
@@ -21,21 +21,21 @@ class User(Base):
     email_address = Column(String)
     created_time = Column(String)
     avatar_location = Column(String)
-    iscompleted = Column(Boolean, default=False)
+    is_completed = Column(Boolean, default=False)
 
 class Tracking(Base):
-    __tablename__ = "trackings"
+    __tablename__ = "tracking"
 
     id = Column(Integer, primary_key=True, index=True)
     created_time = Column(DateTime)
     terminated_time = Column(DateTime)
     is_terminated = Column(Boolean)
-    owner_id = Column(ForeignKey("users.id"), nullable=False)
-    follower_id = Column(ForeignKey("users.id"), nullable=False)
-    challenge_id = Column(ForeignKey('challenges.id'), nullable=False)
+    owner_id = Column(ForeignKey("User.id"), nullable=False)
+    follower_id = Column(ForeignKey("User.id"), nullable=False)
+    challenge_id = Column(ForeignKey('challenge.id'), nullable=False)
 
 class Challenge(Base):
-    __tablename__ = "challenges"
+    __tablename__ = "challenge"
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
@@ -43,16 +43,11 @@ class Challenge(Base):
     duration = Column(Integer)
     breaking_days =  Column(Integer)
     is_public = Column(Boolean, default=False)
-    # Need improvment
     category = Column(String)
     created_time = Column(DateTime)
     cover_location = Column(String)
-    user_id = Column(ForeignKey("users.id"))
-
-class ChallengesStatus(Base):
-    __tablename__ = "challenge_status"
-
-    challenge_id = Column(ForeignKey('challenges.id'), nullable=False, primary_key=True)
+    user_id = Column(ForeignKey("User.id"), nullable=False)
+    course_id = Column(ForeignKey("course.id"))
     is_finished = Column(Boolean)
     days_left = Column(Integer)
     breaking_days_left = Column(Integer)
@@ -61,18 +56,18 @@ class Post(Base):
     __tablename__ = "post"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(ForeignKey("users.id"), nullable=False)
-    challenge_id = Column(ForeignKey("challenges.id"), nullable=False)
+    user_id = Column(ForeignKey("User.id"), nullable=False)
+    challenge_id = Column(ForeignKey("challenge.id"), nullable=False)
     created_time = Column(String)
     start_time = Column(String)
     end_time = Column(String)
-    #the length of content
     written_text = Column(String(1000))
 
 class PostContent(Base):
     __tablename__ = "post_content"
 
-    record_id = Column(ForeignKey("records.id"), nullable=False, primary_key=True)
+    id = Column(Integer, primary_key=True, index=True)
+    post_id = Column(ForeignKey("post.id"), nullable=False, primary_key=True)
     video_location = Column(String)
     image_location = Column(String)
     voice_location = Column(String)
@@ -84,13 +79,12 @@ class Reaction(Base):
     emoji_type = Column(Integer)
     amount = Column(Integer)
     post_id = Column(ForeignKey("post.id"), nullable=False)
-    user_id = Column(ForeignKey("users.id"), nullable=False)
+    user_id = Column(ForeignKey("User.id"), nullable=False)
 
 class Course(Base):
     __tablename__ = "course"
 
     id = Column(Integer, primary_key=True, index=True)
-    # Need a FK with Challenge
     course_name = Column(String, nullable= False)
-    categories = Column(Integer, nullable= False)
+    category = Column(Integer, nullable= False)
     description = Column(String, nullable=False)
