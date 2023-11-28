a853409f0d54e46c538302b23646e12f687a032d
Yaoxing-chen <thewindofstar@gmail.com>
2023-11-28 21:37:03 +1100

    Update to fix syntax error

--- CRUD/challenge.py
+++ CRUD/challenge.py
@@ -48,8 +48,6 @@ def get_challenges_by_course_id(db: Session, course_id: int) -> List[models.Chal
     )
     return challenges_with_course_id
 
-<<<<<<< Updated upstream
-=======
 def get_last_challenge_by_user_id(db: Session, user_id: int):
     last_challenge = (db.query(models.Challenge)
         .filter(models.Challenge.user_id == user_id)
@@ -57,9 +55,6 @@ def get_last_challenge_by_user_id(db: Session, user_id: int):
         )
     return last_challenge
 
-# read all challenges of one user by user id
-# not used in router/challenge.py 
->>>>>>> Stashed changes
 def get_user_challenges(db: Session, user_id: int):
     active_challenges = get_active_challenges_by_user_id(db, user_id)
     finished_challenges = get_finished_challenges_by_user_id(db, user_id)
