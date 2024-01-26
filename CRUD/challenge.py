b65896a0816bfb1a8b0107213ff488809f1c9e06
rick1601 <xwan0122@student.monash.edu>
2024-01-26 15:49:16 +1100

    Update: Timezone string updated now will include "countryName/ before " timezone

--- CRUD/challenge.py
+++ CRUD/challenge.py
@@ -84,8 +84,7 @@ def update_breaking_days_for_specific_challenges(db: Session, timezone_str: str,
     user_timezone = pytz.timezone(TIMEZONE_MAPPING.get(timezone_str, "UTC"))
     # 获取当前时间
     current_time = datetime.now(user_timezone)
-    # 获取当前时间的时区字符串
-    timezone_str_city = timezone_str.split('/')[-1]
+  
     # 获取当前时间的日期字符串
     current_date_str = current_time.strftime('%Y-%m-%d')
 
@@ -108,7 +107,7 @@ def update_breaking_days_for_specific_challenges(db: Session, timezone_str: str,
 
             # 如果组合键不在 Redis 集合中，则减少 breaking_days_left, days_left, 并生成一条用户的帖子记录
             if combo_key not in posted_combinations: 
-                if check_user_timezone.user_timezone == timezone_str_city:
+                if check_user_timezone.user_timezone == timezone_str:
                     if group_member.breaking_days_left > 0:
                         if group_member.days_left > 0:
                             group_member.breaking_days_left -= 1
