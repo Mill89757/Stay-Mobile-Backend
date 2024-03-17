980b280c1cb32bf88ddda637d4a57819a6903f92
YaoxingChen <thewindofstar@gmail.com>
2024-03-17 15:20:49 +1100

    Update: JWT testing

--- Router/challenge.py
+++ Router/challenge.py
@@ -22,6 +22,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 # 依赖项: 解析并验证JWT
 def verify_token(token: str = Depends(oauth2_scheme)):
     try:
+        print(token)
         # 验证JWT
         payload = auth.verify_id_token(token)
         return payload
@@ -360,23 +361,6 @@ async def get_group_challenge_members(challenge_id: int, db:Session = Depends(ge
 async def check_challenge_owner(challenge_id: int, user_id: int, db: Session=Depends(get_db)):
     result = challenge_crud.check_challenge_onwer(challenge_id=challenge_id, user_id=user_id, db=db)
     return result
-
-@router.get("/GetUserChallengesWithToken_Testing/{user_id}", response_model=List[List[schemas.ChallengeWithBreakingDays]])
-async def get_user_challenges_route(user_id: int, db: Session = Depends(get_db)):
-    """read all challenges of one user by user id
-    
-    Args:
-        user_id: id of user
-    
-    Returns:
-        list of active challenges and finished challenges
-    
-    Raises:
-        HTTPException: user not found
-    """
-    active_challenges = challenge_crud.get_active_challenges_by_user_id(db, user_id)
-    finished_challenges = challenge_crud.get_finished_challenges_by_user_id(db, user_id)
-    return [active_challenges, finished_challenges]
 
 # read all challenges of one user by user id 
 @router.get("/GetUserChallengesWithtoken_testing/{user_id}", response_model=List[List[schemas.ChallengeWithBreakingDays]])
